fb96d242c89c9f69f4d5c1775f52dc43ba861729
NgocNhan-DEV <145443104+Jett1-tech@users.noreply.github.com>
2024-10-30 20:20:52 +0700

    Update App.js

--- posturepal_fe/src/App.js
+++ posturepal_fe/src/App.js
@@ -4,8 +4,9 @@ import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-route
 import LandingPage from "./Pages/landingPage";
 import AdminDashboard from "./Components/admin-dashboard/AdminDashboard"
 import Checkout from "./Components/Checkout/Checkout"
+
 import Cancel from "./Cancel"
-const BASE_FE_URL = process.env.REACT_APP_BASE_FE_URL;
+
 function App() {
   return (
     <Router>
@@ -14,10 +15,10 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
 
-          <Route path={`${BASE_FE_URL}/admin/*`} element={<AdminDashboard />} />
-          <Route path={`${BASE_FE_URL}/checkout`} element={<Checkout />} />
+          <Route path="/admin/*" element={<AdminDashboard />} />
+          <Route path="/checkout" element={<Checkout />} />
 
-          <Route path={`${BASE_FE_URL}/api/cancel`} element={<Cancel />} />
+          <Route path="/api/cancel" element={<Cancel />} />
         </Routes>
       </div>
     </Router>
