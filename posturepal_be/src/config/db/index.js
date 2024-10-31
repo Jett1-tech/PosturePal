7817933052e3d7ff09782fb394c4478d43bb9936
NgocNhan-DEV <145443104+Jett1-tech@users.noreply.github.com>
2024-10-31 12:57:38 +0700

    Update index.js

--- posturepal_be/src/config/db/index.js
+++ posturepal_be/src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     const uri =
-      "mongodb+srv://root:123@cluster0.ld5nn.mongodb.net/PosturePal?retryWrites=true&w=majority&appName=Cluster0";
+      "mongodb+srv://root:123@cluster1.gb5f1.mongodb.net/PosturePal?retryWrites=true&w=majority&appName=Cluster1";
     await mongoose.connect(uri);
     console.log("Connect to DB Successfully");
   } catch (error) {
